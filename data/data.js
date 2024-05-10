b98a2426696f904eec7e474484959701e132545e
carlosfariasgaldames <carlosfgaldames@gmail.com>
2024-05-10 12:02:27 -0400

    updated

---
+++ data/data.js
@@ -0,0 +1,29 @@
+export const data = [
+    {
+      question: "1. El desierto más árido del planeta se localiza en Chile, ¿cuál es su nombre?",
+      choices: ["Desierto de Gobi", "Salar del Huasco", " Desierto de Atacama", "Desierto Florido"],
+      answer: "Desierto de Atacama",
+    },
+    {
+      question: "2. ¿Con cuántos países comparte Chile fronteras terrestres?",
+      choices: ["4", "5", "3", "2"],
+      answer: "3",
+    },
+    {
+      question: "3. ¿Cuándo se fundó la ciudad de Santiago?",
+      choices: ["El 6 de agosto de 1538", "El 12 de febrero de 1541", "El 24 de diciembre de 1726", "El 02 de septiembre de 1650",],
+      answer: "El 12 de febrero de 1541",
+    },
+
+    {
+      question: "4. La moneda corriente en Chile es el peso, pero entre 1960 y 1975, circuló una moneda distinta ¿conoces su nombre?",
+      choices: ["El cóndor", " El escudo", " El dólar chileno", "El peso",],
+      answer: "El escudo",
+    },
+
+    {
+      question: "5. ¿En qué año fue Chile el anfitrión de la Copa Mundial de fútbol?",
+      choices: ["1966", "1962", "1963", "1972",],
+      answer: "1962",
+    },
+  ];
